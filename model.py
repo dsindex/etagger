e8ef0ab70a3d624fdabe22bc229a1a7244912bb2
dsindex <hazzling@gmail.com>
2018-07-04 17:41:11 +0900

    fix bugs for multi rnn cell and refactoring

--- model.py
+++ model.py
@@ -7,28 +7,38 @@ from input import *
 import sys
 
 class Model:
+    __keep_prob = 0.5
+    __learning_rate = 0.003
+
     def __init__(self, args):
         self.args = args
+        # Input and Output layer
         self.input_data = tf.placeholder(tf.float32, [None, args.sentence_length, args.word_dim])
         self.output_data = tf.placeholder(tf.float32, [None, args.sentence_length, args.class_size])
-        fw_cell = tf.contrib.rnn.LSTMCell(args.rnn_size, state_is_tuple=True)
-        fw_cell = tf.contrib.rnn.DropoutWrapper(fw_cell, output_keep_prob=0.5)
-        bw_cell = tf.contrib.rnn.LSTMCell(args.rnn_size, state_is_tuple=True)
-        bw_cell = tf.contrib.rnn.DropoutWrapper(bw_cell, output_keep_prob=0.5)
-        fw_cell = tf.contrib.rnn.MultiRNNCell([fw_cell] * args.num_layers, state_is_tuple=True)
-        bw_cell = tf.contrib.rnn.MultiRNNCell([bw_cell] * args.num_layers, state_is_tuple=True)
-        words_used_in_sent = tf.sign(tf.reduce_max(tf.abs(self.input_data), reduction_indices=2))
-        self.length = tf.cast(tf.reduce_sum(words_used_in_sent, reduction_indices=1), tf.int32)
+        # RNN layer
+        fw_cell = tf.contrib.rnn.MultiRNNCell([self.get_a_cell(args.rnn_size, keep_prob=self.__keep_prob) for _ in range(args.num_layers)], state_is_tuple=True)
+        bw_cell = tf.contrib.rnn.MultiRNNCell([self.get_a_cell(args.rnn_size, keep_prob=self.__keep_prob) for _ in range(args.num_layers)], state_is_tuple=True)
+        self.length = self.compute_length(self.input_data)
+        '''
+        [None, args.sentence_length, args.word_dim] -> [args.sentence_length, None, args.word_dim] -> list of [None, args.word_dim]
+        '''
         output, _, _ = tf.contrib.rnn.static_bidirectional_rnn(fw_cell, bw_cell,
                                                tf.unstack(tf.transpose(self.input_data, perm=[1, 0, 2])),
                                                dtype=tf.float32, sequence_length=self.length)
-        weight, bias = self.weight_and_bias(2 * args.rnn_size, args.class_size)
-        output = tf.reshape(tf.transpose(tf.stack(output), perm=[1, 0, 2]), [-1, 2 * args.rnn_size])
-        # [None, 2*args.rnn_size] x [2*args.rnn_size, args.class_size]  -> [None, args.class_size]
-        prediction = tf.nn.softmax(tf.matmul(output, weight) + bias) 
+        output = tf.reshape(tf.transpose(tf.stack(output), perm=[1, 0, 2]), [-1, 2*args.rnn_size])
+        # Fully connected and Softmax layer
+        weight, bias = self.weight_and_bias(2*args.rnn_size, args.class_size)
+        '''
+        [None, 2*args.rnn_size] x [2*args.rnn_size, args.class_size]  -> [None, args.class_size]
+        '''
+        prediction = tf.nn.softmax(tf.matmul(output, weight) + bias)
+        '''
+        [None, args.class_size] -> [None, args.sentence_length, args.class_size]
+        '''
         self.prediction = tf.reshape(prediction, [-1, args.sentence_length, args.class_size])
+        # Loss and optimization
         self.loss = self.cost()
-        optimizer = tf.train.AdamOptimizer(0.003)
+        optimizer = tf.train.AdamOptimizer(self.__learning_rate)
         tvars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), 10)
         self.train_op = optimizer.apply_gradients(zip(grads, tvars))
@@ -36,12 +46,24 @@ class Model:
     def cost(self):
         cross_entropy = self.output_data * tf.log(self.prediction)
         cross_entropy = -tf.reduce_sum(cross_entropy, reduction_indices=2)
+        # for ignoring padding region, use mask
         mask = tf.sign(tf.reduce_max(tf.abs(self.output_data), reduction_indices=2))
-        # for ignoring padding region
         cross_entropy *= mask
         cross_entropy = tf.reduce_sum(cross_entropy, reduction_indices=1)
         cross_entropy /= tf.cast(self.length, tf.float32)
         return tf.reduce_mean(cross_entropy)
+
+    @staticmethod
+    def get_a_cell(rnn_size, keep_prob):
+        cell = tf.contrib.rnn.LSTMCell(rnn_size, state_is_tuple=True)
+        drop = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+        return drop
+
+    @staticmethod
+    def compute_length(input_data):
+        words_used_in_sent = tf.sign(tf.reduce_max(tf.abs(input_data), reduction_indices=2))
+        length = tf.cast(tf.reduce_sum(words_used_in_sent, reduction_indices=1), tf.int32)
+        return length
 
     @staticmethod
     def weight_and_bias(in_size, out_size):
