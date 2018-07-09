0bbe0f0cb1233464f2c4184898785897afb9d48b
dsindex <hazzling@gmail.com>
2018-07-09 17:36:42 +0900

    evaluate predicted output

---
+++ eval.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import numpy as np
+
+import sys
+import argparse
+
+cls = {}
+tp = {}
+fp = {}
+fn = {}
+precision = {}
+recall = {}
+fscore = {}
+
+def _eval(bucket):
+    for line in bucket:
+        tokens = line.split()
+        size = len(tokens)
+        assert(size == 5)
+        w = tokens[0]
+        pos = tokens[1]
+        chunk = tokens[2]
+        tag = tokens[3]
+        pred = tokens[4]
+        ctag = 'O'
+        if '-' in tag: ctag = tag.split('-')[1]
+        if pred not in tp: tp[pred] = 0
+        if pred not in fp: fp[pred] = 0
+        if ctag not in fn: fn[ctag] = 0
+        if ctag == pred:
+            tp[pred] += 1
+        else:
+            fp[pred] += 1
+            fn[ctag] += 1
+        cls[pred] = None
+        cls[ctag] = None
+
+def eval(args):
+    bucket = []
+    while 1:
+        try: line = sys.stdin.readline()
+        except KeyboardInterrupt: break
+        if not line: break
+        line = line.strip()
+        if not line and len(bucket) >= 1:
+            _eval(bucket)
+            bucket = []
+        if line : bucket.append(line)
+    if len(bucket) != 0:
+        _eval(bucket)
+
+    print(tp)
+    print(fp)
+    print(fn)
+
+    for c, _ in cls.iteritems():
+        precision[c] = tp[c]*1.0 / (tp[c] + fp[c])
+        recall[c] = tp[c]*1.0 / (tp[c] + fn[c])
+        fscore[c] = 2.0*precision[c]*recall[c] / (precision[c] + recall[c])
+
+    print('')
+    print('precision:')
+    for c, _ in precision.iteritems():
+        print(c + ',' + str(precision[c]))
+    print('')
+    print('recall:')
+    for c, _ in recall.iteritems():
+        print(c + ',' + str(recall[c]))
+    print('')
+    print('fscore:')
+    for c, _ in fscore.iteritems():
+        print(c + ',' + str(fscore[c]))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+
+    eval(args)
