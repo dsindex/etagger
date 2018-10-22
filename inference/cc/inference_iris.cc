beb4dcd0efae9c6a6c21bb341f841d5b9582a3ca
dsindex <hazzling@gmail.com>
2018-10-22 18:57:14 +0900

    -

--- inference/cc/inference_iris.cc
+++ inference/cc/inference_iris.cc
@@ -1,4 +1,4 @@
-// 2018, Patrick Wieschollek <mail@patwie.com>
+// source template is from https://github.com/PatWie/tensorflow-cmake/blob/master/inference/cc/inference_cc.cc
 #include <tensorflow/core/protobuf/meta_graph.pb.h>
 #include <tensorflow/core/public/session.h>
 #include <tensorflow/core/public/session_options.h>
@@ -7,28 +7,6 @@
 
 typedef std::vector<std::pair<std::string, tensorflow::Tensor>> tensor_dict;
 
-/**
- * @brief load a previous store model
- * @details [long description]
- *
- * in Python run:
- *
- *    saver = tf.train.Saver(tf.global_variables())
- *    saver.save(sess, './exported/my_model')
- *    tf.train.write_graph(sess.graph, '.', './exported/graph.pb, as_text=False)
- *
- * this relies on a graph which has an operation called `init` responsible to
- * initialize all variables, eg.
- *
- *    sess.run(tf.global_variables_initializer())  # somewhere in the python
- * file
- *
- * @param sess active tensorflow session
- * @param graph_fn path to graph file (eg. "./exported/graph.pb")
- * @param checkpoint_fn path to checkpoint file (eg. "./exported/my_model",
- * optional)
- * @return status of reloading
- */
 tensorflow::Status LoadModel(tensorflow::Session *sess, std::string graph_fn,
                              std::string checkpoint_fn = "") {
   tensorflow::Status status;
@@ -68,8 +46,8 @@ tensorflow::Status LoadModel(tensorflow::Session *sess, std::string graph_fn,
 }
 
 int main(int argc, char const *argv[]) {
-  const std::string graph_fn = "./exported/my_model.meta";
-  const std::string checkpoint_fn = "./exported/my_model";
+  const std::string graph_fn = "./exported/iris_model.meta";
+  const std::string checkpoint_fn = "./exported/iris_model";
 
   // prepare session
   tensorflow::Session *sess;
@@ -78,25 +56,23 @@ int main(int argc, char const *argv[]) {
   TF_CHECK_OK(LoadModel(sess, graph_fn, checkpoint_fn));
 
   // prepare inputs
-  tensorflow::TensorShape data_shape({1, 2});
+  tensorflow::TensorShape data_shape({1, 4});
   tensorflow::Tensor data(tensorflow::DT_FLOAT, data_shape);
-
-  // same as in python file
   auto data_ = data.flat<float>().data();
-  for (int i = 0; i < 2; ++i) data_[i] = 1;
-
+  data_[0] = 2;
+  data_[1] = 14;
+  data_[2] = 33;
+  data_[3] = 50;
   tensor_dict feed_dict = {
-      {"input", data},
+      {"X", data},
   };
 
   std::vector<tensorflow::Tensor> outputs;
-  TF_CHECK_OK(sess->Run(feed_dict, {"output", "dense/kernel:0", "dense/bias:0"},
+  TF_CHECK_OK(sess->Run(feed_dict, {"logits"},
                         {}, &outputs));
 
   std::cout << "input           " << data.DebugString() << std::endl;
-  std::cout << "output          " << outputs[0].DebugString() << std::endl;
-  std::cout << "dense/kernel:0  " << outputs[1].DebugString() << std::endl;
-  std::cout << "dense/bias:0    " << outputs[2].DebugString() << std::endl;
+  std::cout << "logits          " << outputs[0].DebugString() << std::endl;
 
   return 0;
 }
