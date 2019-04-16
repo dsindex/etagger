85a6acbcb9d4e85b11a9f3264d3b760548b783ad
dsindex <hazzling@gmail.com>
2019-04-16 11:04:14 +0900

    add demo page

--- inference/www/static/js/application.js
+++ inference/www/static/js/application.js
@@ -8,18 +8,6 @@ $(document).ready(function() {
 				method: 'POST'
 			}).done(function(data) {
 				if (data.success) {
-					if(data.filename) {
-						d = new Date();
-						$('#graph').attr('src', data.filename + "?" + d.getTime());
-					}
-					if(data.pgraph) {
-						setTimeout( function() {
-							var iframe = $('#pgraph');
-							var doc = iframe[0].contentWindow.document;
-							var body = $('body',doc);
-							body.html(data.pgraph);
-						}, 1 );
-					}
 					$('#info').empty();
 					if(data.info) {
 						$('textarea#info').text(data.info);
@@ -29,29 +17,20 @@ $(document).ready(function() {
 						var trHTML = '';
 						trHTML += "<tr class=\"success\">";
 						trHTML += "<th>id</th>";
-						trHTML += "<th>form</th>";
-						trHTML += "<th>lemma</th>";
-						trHTML += "<th>upostag</th>";
-						trHTML += "<th>xpostag</th>";
-						trHTML += "<th>feats</th>";
-						trHTML += "<th>head</th>";
-						trHTML += "<th>deprel</th>";
-						trHTML += "<th>deps</th>";
-						trHTML += "<th>misc</th>";
+						trHTML += "<th>word</th>";
+						trHTML += "<th>pos</th>";
+						trHTML += "<th>chk</th>";
+						trHTML += "<th>_tag</th>";
+						trHTML += "<th>tag</th>";
 						trHTML += "</tr>";
 						$.each(data.record, function (i, entry) {
 							$.each(entry, function (j, item) {
 								trHTML += '<tr><td>' + item.id;
-								if( item.head == '0' ) item.form = '<font color=Blue>' + item.form + '</font>';
-								trHTML += '</td><td>' + item.form;
-								trHTML += '</td><td>' + item.lemma;
-								trHTML += '</td><td>' + item.upostag;
-								trHTML += '</td><td>' + item.xpostag;
-								trHTML += '</td><td>' + item.feats;
-								trHTML += '</td><td>' + item.head;
-								trHTML += '</td><td>' + item.deprel;
-								trHTML += '</td><td>' + item.deps;
-								trHTML += '</td><td>' + item.misc;
+								trHTML += '</td><td>' + item.word;
+								trHTML += '</td><td>' + item.pos;
+								trHTML += '</td><td>' + item.chk;
+								trHTML += '</td><td>' + item._tag;
+								trHTML += '</td><td>' + item.tag;
 								trHTML += '</td></tr>';
 							});
 							trHTML += '<tr></tr>';
